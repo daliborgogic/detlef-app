e1906e7e62fb1c89c2bfb9f8734845bb4228511a
dlbr <mail@daliborgogic.com>
2018-08-14 15:17:19 +0200

    font loading module level 3

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,26 @@ div
 import TheHeader from '@/components/TheHeader'
 
 export default {
-  components: { TheHeader }
+  components: { TheHeader },
+
+  beforeMount () { this.loadFont() },
+
+  methods: {
+    // CSS Font Loading Module Level 3
+    async loadFont () {
+      const font = new FontFace('Custom Font', 'url(./fonts/custom.woff)')
+      await font.load()
+
+      document.fonts.add(font)
+      document.getElementById('__nuxt').classList.add('font-loaded')
+    }
+  }
 }
 </script>
+
+<style lang="stylus">
+#__nuxt
+  font-family -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif
+#__nuxt.font-loaded
+  font-family "Custom Font"
+</style>
