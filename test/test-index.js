480bcf0f92fb8998df5de3504255d9f109807100
dlbr <mail@daliborgogic.com>
2018-08-31 14:54:26 +0200

    test route

---
+++ test/test-index.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+import micro from 'micro'
+import listen from 'test-listen'
+import request from 'request-promise'
+import { Nuxt, Builder } from 'nuxt-edge'
+import config  from './../nuxt.config.js'
+
+const { APP_NAME } = process.env
+
+const nuxt = new Nuxt(config)
+
+const getUrl = fn => {
+  const srv = micro(fn)
+
+  return listen(srv)
+}
+
+test('Route / exits and render HTML', async t => {
+  const fn = async (req, res) => {
+    const html  = await nuxt.render(req, res)
+    return html
+  }
+  const url = await getUrl(fn)
+  const res = await request(url)
+
+  t.true(res.includes(APP_NAME))
+})
