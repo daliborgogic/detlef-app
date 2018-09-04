f024426f430849620552e6aafad174bcd44cc183
dlbr <mail@daliborgogic.com>
2018-09-04 19:48:06 +0200

    use manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ module.exports = {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
-      { rel: 'manifest', href: '/icons/site.webmanifest' },
+      { rel: 'manifest', href: '/icons/manifest.json' },
       { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#000000' },
       { rel: 'shortcut icon', href: '/icons/favicon.ico' }
     ]
